564849744f60165c5d49367a2b8f05922c32938f
Diego <diegonrriqueaz123@hotmail.com>
2023-08-16 20:13:53 -0300

    1# acrescentado front end em conjunto com bootstrap e jquery

--- adicionar/index.php
+++ adicionar/index.php
@@ -6,9 +6,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="adicionar/public/inserirFolhaEstilo.css">
+    <script src="../jquery/code.jquery.com_jquery-3.5.1.min.js"></script>
+    <script src="../bootstrap-4.0.0/js/bootstrap.min.js"></script>
     <title>Adicionar produto</title>
 </head>
-<body>
+<body class="text-center bg-primary">
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto d-flex justify-content-center">
+        <div class="inner bg-white justify-content-center" style="width: 700px;border-radius: 15px;">
+            <h3 class="masthead-brand">RG</h3>
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link linkHover navLink noSelected" style="text-decoration: none;" href="#">Home</a>
+                <a class="nav-link linkHover active2 navLink" style="text-decoration: none;" href="#">Adicionar</a>
+            </nav>
+        </div>
+    </header>
+
+    <main role="main" class="inner cover">
+        <h1 class="cover-heading">Adicionar Produto</h1>
+        <input>
+        <input>
+        <input>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+    </footer>
+</div>
 
 </body>
+<script>
+
+</script>
 </html>
