fa938a7b3906bf97828c31448f1e03220d25b17f
Diego Henrique <diego.hc.sampaio@gmail.com>
2023-10-03 08:12:52 -0300

    First visual barber

--- site/cortar/main.php
+++ site/cortar/main.php
@@ -1,2 +1,43 @@
-<?php
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../bootstrap-4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/main.css">
+    <link href="../../fontawesome-6.4.2/css/fontawesome.min.css" rel="stylesheet">
+    <link href="../../fontawesome-6.4.2/css/brands.css" rel="stylesheet">
+    <link href="../../fontawesome-6.4.2/css/solid.css" rel="stylesheet">
+    <script src="../../jquery/code.jquery.com_jquery-3.5.1.min.js"></script>
+    <script src="../../bootstrap-4.0.0/js/bootstrap.min.js"></script>
+    <script src="../../maskMoney/src/jquery.maskMoney.js" type="text/javascript"></script>
+    <title>Cortar</title>
+</head>
+<body class="text-center" >
+<div class=" d-flex flex-row" style="padding: 70px;height: 800px;">
+    <div class="mr-2" style="padding: 0px 15px;width:40%;margin-top:20%;">
+        <div>
+            <button class="btn" id="buttonCorte">Cortar</button>
+        </div>
+    </div>
+    <div id="body_menu" >
+        <div class="d-flex align-content-end" style="width: 100%;">
+            <div class="d-flex flex-column" style="width: 40%;">
+                <span class="bg_letter_primary">Serviços</span>
+                <span>Option1</span>
+                <span>Option2</span>
+            </div>
+            <div class="d-flex flex-column" style="width: 50%;">
+                <span class="bg_letter_primary">Produtos</span>
+                <span>Option1</span>
+                <span>Option2</span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script>
+
+</script>
+</html>
